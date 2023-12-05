062c5eaebe24e514e101bb5317eb1db325bf995d
Thomas Jepp <tom@tomjepp.co.uk>
2023-12-05 22:23:12 +0000

    Day 4

---
+++ Day04.Part1/Program.cs
@@ -0,0 +1,101 @@
+﻿using System.Diagnostics;
+using System.Text.RegularExpressions;
+
+var stopwatch = new Stopwatch();
+
+stopwatch.Start();
+
+var regex = new Regex(@"^Card\s+(\d+): (.+?) \| (.+?)$", RegexOptions.Compiled);
+var cards = new List<Card>();
+
+using (var reader = File.OpenText("input.txt"))
+{
+    while (!reader.EndOfStream)
+    {
+        var line = reader.ReadLine();
+        if (line == null || string.IsNullOrWhiteSpace(line))
+        {
+            continue;
+        }
+
+        // parse your input here
+        var match = regex.Match(line);
+        if (!match.Success)
+        {
+            continue;
+        }
+
+        int cardId = int.Parse(match.Groups[1].Value);
+        var winningNumberStrings = match.Groups[2].Value
+            .Split(' ', StringSplitOptions.TrimEntries | StringSplitOptions.RemoveEmptyEntries);
+        var winningNumbers = new List<int>();
+        foreach (var winningNumberString in winningNumberStrings)
+        {
+            winningNumbers.Add(int.Parse(winningNumberString));
+        }
+        var numberOnCardStrings = match.Groups[3].Value
+            .Split(' ', StringSplitOptions.TrimEntries | StringSplitOptions.RemoveEmptyEntries);
+        var numbersOnCard = new List<int>();
+        foreach (var numberOnCardString in numberOnCardStrings)
+        {
+            numbersOnCard.Add(int.Parse(numberOnCardString));
+        }
+
+        var card = new Card()
+        {
+            Id = cardId,
+            WinningNumbers = winningNumbers,
+            NumbersOnCard = numbersOnCard,
+        };
+        cards.Add(card);
+        //Console.WriteLine($"{line} - score: {card.Score}");
+    }
+}
+
+stopwatch.Stop();
+var parsedIn = stopwatch.Elapsed;
+stopwatch.Restart();
+
+// calculate & output your result here
+var totalScore = cards.Sum(x => x.Score);
+Console.WriteLine(totalScore);
+
+stopwatch.Stop();
+var processedIn = stopwatch.Elapsed;
+
+Console.WriteLine();
+Console.WriteLine($"parsing time: {parsedIn.TotalMilliseconds:0.####} milliseconds");
+Console.WriteLine($"processing time: {processedIn.TotalMilliseconds:0.####} milliseconds");
+
+class Card
+{
+    public int Id { get; set; }
+    public List<int> WinningNumbers { get; set; }
+    public List<int> NumbersOnCard { get; set; }
+
+    public long Score
+    {
+        get
+        {
+            long score = 0;
+            foreach (var numberOnCard in NumbersOnCard)
+            {
+                if (!WinningNumbers.Contains(numberOnCard))
+                {
+                    continue;
+                }
+
+                if (score == 0)
+                {
+                    score = 1;
+                }
+                else
+                {
+                    score *= 2;
+                }
+            }
+
+            return score;
+        }
+    }
+}
---
+++ Day04.Part2/Program.cs
@@ -0,0 +1,107 @@
+﻿using System.Collections;
+using System.Diagnostics;
+using System.Text.RegularExpressions;
+
+var stopwatch = new Stopwatch();
+
+stopwatch.Start();
+
+var regex = new Regex(@"^Card\s+(\d+): (.+?) \| (.+?)$", RegexOptions.Compiled);
+var cards = new Dictionary<int, Card>();
+var outstandingCards = new Queue<Card>();
+
+using (var reader = File.OpenText("input.txt"))
+{
+    while (!reader.EndOfStream)
+    {
+        var line = reader.ReadLine();
+        if (line == null || string.IsNullOrWhiteSpace(line))
+        {
+            continue;
+        }
+
+        // parse your input here
+        var match = regex.Match(line);
+        if (!match.Success)
+        {
+            continue;
+        }
+
+        int cardId = int.Parse(match.Groups[1].Value);
+        var winningNumberStrings = match.Groups[2].Value
+            .Split(' ', StringSplitOptions.TrimEntries | StringSplitOptions.RemoveEmptyEntries);
+        var winningNumbers = new List<int>();
+        foreach (var winningNumberString in winningNumberStrings)
+        {
+            winningNumbers.Add(int.Parse(winningNumberString));
+        }
+        var numberOnCardStrings = match.Groups[3].Value
+            .Split(' ', StringSplitOptions.TrimEntries | StringSplitOptions.RemoveEmptyEntries);
+        var numbersOnCard = new List<int>();
+        foreach (var numberOnCardString in numberOnCardStrings)
+        {
+            numbersOnCard.Add(int.Parse(numberOnCardString));
+        }
+
+        var card = new Card()
+        {
+            Id = cardId,
+            WinningNumbers = winningNumbers,
+            NumbersOnCard = numbersOnCard,
+        };
+        cards.Add(cardId, card);
+        outstandingCards.Enqueue(card);
+        //Console.WriteLine($"{line} - score: {card.Score}");
+    }
+}
+
+stopwatch.Stop();
+var parsedIn = stopwatch.Elapsed;
+stopwatch.Restart();
+
+int totalCards = 0;
+// calculate & output your result here
+while (outstandingCards.Count > 0)
+{
+    var card = outstandingCards.Dequeue();
+    for (int i = card.Id + 1; i < card.Id + 1 + card.Score; i++)
+    {
+        outstandingCards.Enqueue(cards[i]);
+    }
+
+    totalCards++;
+}
+Console.WriteLine(totalCards);
+
+stopwatch.Stop();
+var processedIn = stopwatch.Elapsed;
+
+Console.WriteLine();
+Console.WriteLine($"parsing time: {parsedIn.TotalMilliseconds:0.####} milliseconds");
+Console.WriteLine($"processing time: {processedIn.TotalMilliseconds:0.####} milliseconds");
+
+class Card
+{
+    public int Id { get; set; }
+    public List<int> WinningNumbers { get; set; }
+    public List<int> NumbersOnCard { get; set; }
+
+    public long Score
+    {
+        get
+        {
+            long score = 0;
+            foreach (var numberOnCard in NumbersOnCard)
+            {
+                if (!WinningNumbers.Contains(numberOnCard))
+                {
+                    continue;
+                }
+
+                score++;
+            }
+
+            return score;
+        }
+    }
+}
